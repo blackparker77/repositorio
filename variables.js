6b5273c968f89a0cc72e6a54fb36a720b0a92cf7
Carlos Pallares <blackparker77@gmail.com>
2020-05-27 20:08:51 -0500

    primer commit en js

---
+++ variables.js
@@ -0,0 +1,13 @@
+/* var name = "Carlos"; // cámbialo por tu nombre
+console.log("Hola " + name);
+
+var medida = 10;
+console.log("El perimetro de un cuadro de lado " + medida + " es " + (medida *4));
+
+name= "lname";
+console.log("hola " + lname); */
+
+var name= "alex";
+var age= 19;
+var country="mexico";
+console.log(name + " tiene " + age + " años " + "y vive en " + country);
